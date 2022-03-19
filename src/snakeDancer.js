47edfef0b42f029ab5e1e3a13b1e6c3bcec164f6
Danting Huang <serena.huangdanting@gmail.com>
2022-03-19 01:06:41 -0400

    Edit snakeDancer movement

--- src/snakeDancer.js
+++ src/snakeDancer.js
@@ -8,17 +8,19 @@ makeSnakeDancer.prototype = Object.create(makeDancer.prototype);
 makeSnakeDancer.prototype.constructor = makeSnakeDancer;
 makeSnakeDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  $('.snakeDancer').animate({ 'left': '-=100px' }, 'slow' );
-  $('.snakeDancer').animate({ 'top': '-=100px' }, 'slow' );
-  $('.snakeDancer').animate({ 'left': '+=100px' }, 'slow' );
-  $('.snakeDancer').animate({ 'top': '-=100px' }, 'slow' );
-  $('.snakeDancer').animate({ 'left': '-=100px' }, 'slow' );
-  $('.snakeDancer').animate({ 'top': '+=100px' }, 'slow' );
-  $('.snakeDancer').animate({ 'left': '+=100px' }, 'slow' );
-  $('.snakeDancer').animate({ 'top': '+=100px' }, 'slow' );
+  $('.snakeDancer').animate({ 'left': '-=50px' }, 'slow' );
+  $('.snakeDancer').animate({ 'top': '-=50px' }, 'slow' );
+  $('.snakeDancer').animate({ 'left': '+=50px' }, 'slow' );
+  $('.snakeDancer').animate({ 'top': '-=50px' }, 'slow' );
+  $('.snakeDancer').animate({ 'left': '-=50px' }, 'slow' );
+  $('.snakeDancer').animate({ 'top': '+=50px' }, 'slow' );
+  $('.snakeDancer').animate({ 'left': '+=50px' }, 'slow' );
+  $('.snakeDancer').animate({ 'top': '+=50px' }, 'slow' );
 };
 
 makeSnakeDancer.prototype.lineUp = function() {
+
+
 
   this.$node.css({
     'top': '60%'
